5871e0909d524926d01e1dc5fdd6d93376bf389f
Gorik <smartpneu.contact@gmail.com>
2026-03-14 22:53:59 -0400

    Initial Commit

---
+++ SmartPneu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartPneu
+//
+//  Created by Arsen Khanguieldyan on 3/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartPneu/SmartPneuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPneuApp.swift
+//  SmartPneu
+//
+//  Created by Arsen Khanguieldyan on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartPneuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
